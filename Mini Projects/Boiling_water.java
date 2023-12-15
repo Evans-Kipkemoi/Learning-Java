7c5c0e4084e70e12ddfb633aa546c348359dc294
Evans-Kipkemoi <evanskipkemoi7@gmail.com>
2023-12-15 05:56:09 +0300

    Program takes the integer temperatur in Celsius and outputs Boiling if temperature is above or equal to 100. Else outputs Not boiling

---
+++ Mini Projects/Boiling_water.java
@@ -0,0 +1,17 @@
+import  java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int temperature = sc.nextInt();
+        
+        if (temperature >= 100) {
+            System.out.println("Boiling");
+        }
+        else {
+            System.out.println("Not boiling");
+        }
+        
+        
+    }
+}
