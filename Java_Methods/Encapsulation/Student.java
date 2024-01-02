dd14dd72c06a41961e1dc2f42c59fbbdc7b3fd85
Evans-Kipkemoi <evanskipkemoi7@gmail.com>
2024-01-02 12:53:36 +0300

    Adding key files

---
+++ Java_Methods/Encapsulation/Student.java
@@ -0,0 +1,26 @@
+public class Student { 
+	private String name;
+	private int age;
+
+	// Setter
+	public void setName(String newName){
+		name = newName;
+
+	}
+
+	// Getter
+	public String getName(){
+		return name;
+	}
+
+	// Setter
+	public void setAge(int newAge) {
+		age = newAge;
+	}
+
+	// Getter
+	public int geAge() {
+		return age;
+	}
+
+}
