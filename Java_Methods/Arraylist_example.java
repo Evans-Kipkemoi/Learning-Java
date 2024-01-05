76f5deef0e54fe048c71590600340398cc062079
Evans-Kipkemoi <evanskipkemoi7@gmail.com>
2024-01-05 08:33:27 +0300

    Array list

---
+++ Java_Methods/Arraylist_example.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Main { 
+  public static void main(String[] args) { 
+    ArrayList<String> fruits = new ArrayList<String>();
+    fruits.add("Oranges");
+    fruits.add("Apples");
+    fruits.add("Bananas");
+    fruits.add("Pineapples");
+    System.out.println(fruits);
+
+    //fruits.get(0); Accesss an item
+    //fruits.set(0, "Avocadoes"); Change item
+    //fruits.remove(0); remove an item
+    //fruits.clear(); Remove all elements
+
+
+  } 
+}
